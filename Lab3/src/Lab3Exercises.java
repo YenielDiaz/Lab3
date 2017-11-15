574fa4be42ae00011cbf9a4418aef2964e85d96d
4010a4 <4010a4@zapote.ece.uprm.edu>
2017-11-15 12:14:37 -0400

    Decreased size of Jframe and centralized it.
    Changed the name and size of the Jframe. Aditionally, made it appear
    aproximately at the center of the display.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,10 +2,10 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
-		myFrame.setVisible(true);
+		JFrame Yeniel = new JFrame("Hello, world!");
+		Yeniel.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		Yeniel.setLocation(850, 400);
+		Yeniel.setSize(200, 200);
+		Yeniel.setVisible(true);
 	}
 }
