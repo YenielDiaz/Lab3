540d082fd9c3beaf4976523f2a88e19404ef972d
Yeniel <Yeniel@GarageDoor>
2017-11-16 17:56:13 -0800

    Drew PR flag
    Added different polygons which together make a Puerto Rican flag.
    However, it's pretty hard coded. It does not work on different sizes
    ofthe Jframe

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -7,7 +7,7 @@ public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame Yeniel = new JFrame("Hello, world!");
 		Yeniel.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		Yeniel.setSize(200, 200);
+		Yeniel.setSize(220, 200);
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 		Yeniel.setLocation(dim.width/2-Yeniel.getSize().width/2, dim.height/2-Yeniel.getSize().height/2);
 		MyPanelClass myPanel = new MyPanelClass();
--- Lab3/src/MyPanelClass.java
+++ Lab3/src/MyPanelClass.java
@@ -24,7 +24,7 @@ public class MyPanelClass extends JPanel{
         int height = y2 - y1;
 
        //Paint the background
-        g.setColor(Color.darkGray);
+        g.setColor(Color.WHITE);
         g.fillRect(x1, y1, width + 1, height + 1);
 //        //Draw a border
 //        g.setColor(Color.YELLOW);
@@ -42,6 +42,8 @@ public class MyPanelClass extends JPanel{
         
 //       g.setColor(Color.LIGHT_GRAY);
 //       g.fillOval(getWidth()/2 - 55/2, getHeight()/2 - 55/2, 55, 55);
+        
+        
 //        Polygon p = new Polygon();
 //        p.addPoint(x1 + 5, y1 + 25);
 //        p.addPoint(x1 + 20, y1 + 10);
@@ -52,19 +54,51 @@ public class MyPanelClass extends JPanel{
 //        p.addPoint(x1 + 15, y1 + 25);
 //        g.setColor(Color.YELLOW);
 //        g.fillPolygon(p);
-        Polygon p2 = new Polygon();
-        p2.addPoint(x1 + 25, y1 + 73);
-        p2.addPoint(x1 + 41, y1 + 73);
-        p2.addPoint(x1 + 47, y1 + 58);
-        p2.addPoint(x1 + 53, y1 + 73);
-        p2.addPoint(x1 + 69, y1 + 73);
-        p2.addPoint(x1 + 56, y1 + 83);
-        p2.addPoint(x1 + 61, y1 + 98);
-        p2.addPoint(x1 + 47, y1 + 88);
-        p2.addPoint(x1 + 34, y1 + 98);
-        p2.addPoint(x1 + 38, y1 + 83);
+        
+        Polygon stripe1 = new Polygon();
+        stripe1.addPoint(x1, y1);
+        stripe1.addPoint(width, y1);
+        stripe1.addPoint(width, y1 + 30);
+        stripe1.addPoint(x1 + 41, y1 + 30);
+        g.setColor(Color.RED);
+        g.fillPolygon(stripe1);
+        
+        Polygon stripe2 = new Polygon();
+        stripe2.addPoint(x1 + 85, y1 + 60);
+        stripe2.addPoint(width, y1 + 60);
+        stripe2.addPoint(width, y1 + 95);
+        stripe2.addPoint(x1 + 95,  y1 + 95);
+        g.fillPolygon(stripe2);
+        
+        Polygon stripe3 = new Polygon();
+        stripe3.addPoint(x1 + 30, height - 30);
+        stripe3.addPoint(width, height - 30);
+        stripe3.addPoint(width, height);
+        stripe3.addPoint(x1, height);
+        g.fillPolygon(stripe3);
+        
+        Polygon tri = new Polygon();
+        tri.addPoint(x1, y1);
+        tri.addPoint(x1 + 115, y1 + 80);
+        tri.addPoint(0, height);
+        g.setColor(Color.BLUE);
+        g.fillPolygon(tri);
+        
+        Polygon star = new Polygon();
+        star.addPoint(x1 + 25, y1 + 73);
+        star.addPoint(x1 + 41, y1 + 73);
+        star.addPoint(x1 + 47, y1 + 58);
+        star.addPoint(x1 + 53, y1 + 73);
+        star.addPoint(x1 + 69, y1 + 73);
+        star.addPoint(x1 + 56, y1 + 83);
+        star.addPoint(x1 + 61, y1 + 98);
+        star.addPoint(x1 + 47, y1 + 88);
+        star.addPoint(x1 + 34, y1 + 98);
+        star.addPoint(x1 + 38, y1 + 83);
         g.setColor(Color.WHITE);
-        g.drawPolygon(p2);
+        g.fillPolygon(star);
+        
+        
 	}
 }
 	
